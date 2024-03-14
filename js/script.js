c35bc510dbdf2c4c68e4a39f8a2a1a2f4915b040
joao <jmsalim@knights.ucf.edu>
2024-03-14 17:05:23 -0400

    updates

--- js/script.js
+++ js/script.js
@@ -8,15 +8,6 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-document.getElementById('hamburger').addEventListener('click', function() {
-    var nav = document.getElementById('navbar');
-    if (nav.style.display === 'block') {
-        nav.style.display = 'none';
-    } else {
-        nav.style.display = 'block';
-    }
-});
-
 document.querySelectorAll('.project-link').forEach(link => {
     link.addEventListener('click', function(e) {
         console.log('Link clicked:', this.href); // Add this line
